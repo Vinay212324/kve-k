185124179224214a26f345e97b753a648312e999
Vinay <166391124+Vinay212324@users.noreply.github.com>
2025-07-27 20:04:25 +0530

    Update __manifest__.py

--- kve/custom_login_page_user/__manifest__.py
+++ kve/custom_login_page_user/__manifest__.py
@@ -1,12 +1,14 @@
-{ 
-    "name": "Custom Login Page User", 
-    "version": "16.0.1.0.0", 
-    "summary": "Fully custom login page with separate template", 
-    "depends": ["web"], 
-    "data": ["views/login_template.xml"], 
+{
+    "name": "Custom Login Page User",
+    "version": "17.0.1.0.0",
+    "summary": "Fully custom login page with separate template",
+    "depends": ["web"],
+    "data": ["views/login_template.xml"],
     "assets": {
-        "web.assets_frontend": ["custom_login_page_user/static/src/css/login.css"]
-    }, 
-    "installable": True, 
-    "auto_install": False 
+        "web.assets_frontend": [
+            "custom_login_page_user/static/src/css/login.css",
+        ],
+    },
+    "installable": True,
+    "auto_install": True,
 }
